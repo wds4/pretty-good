2aaf2f693971ae310ea54d6cd1b8adb16006162d
David Strayhorn <david.strayhorn4@gmail.com>
2023-02-03 12:00:00 -0500

    changes to direct messages filter

--- src/renderer/window1/apps/nostr/directMessages/messagesReceived.tsx
+++ src/renderer/window1/apps/nostr/directMessages/messagesReceived.tsx
@@ -9,12 +9,16 @@ const DirectMessagesReceived = () => {
   // 3bf0c63fcb93463407af97a5e5ee64fa883d107ef9e558472c4eb9aaaefa459d - fiatjaf
   // 84dee6e676e5bb67b4ad4e042cf70cbd8681155db535942fcc6a0533858a7240 - snowden
   const filter = {
-    "#p": [ "32e1827635450ebb3c5a7d12c1f8e7b2b514439ac10a67eef3d9fd9c5c68e245" ],
     since: 0,
     kinds: [4],
+    "#p": [ myPubkey ],
   },
-  const { events } = useNostrEvents((filter)=>{
-    filter: filter
+  const { events } = useNostrEvents({
+    filter: {
+      since: 0,
+      kinds: [4],
+      "#p": [ myPubkey ],
+    }
   });
   return (
     <>
