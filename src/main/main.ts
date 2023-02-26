001a9c48b2c01324a893c8353728a75cac699b2a
David Strayhorn <david.strayhorn4@gmail.com>
2023-02-26 01:46:15 -0500

    comment out AppUpdater

--- src/main/main.ts
+++ src/main/main.ts
@@ -298,7 +298,7 @@ const createWindows = async () => {
 
   // Remove this if your app does not use auto updates
   // eslint-disable-next-line
-  new AppUpdater();
+  // new AppUpdater();
 };
 
 /**
